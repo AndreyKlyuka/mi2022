f1669b56c3970ad50b70086ba747ddb437804ba0
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-07-27 23:12:11 +0300

    refactor: rework contentsection component, now use props.childern to show difference sorting fields for difference pages

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -25,30 +25,4 @@ export const ContentSection = (props) => {
       )}
     </div>
   )
-
-  // if (props.currentPage === 'breeds')
-  //   return (
-  //     <div className="content ">
-  //       <div className="flex ">
-  //         <div className="button-back">
-  //           <img src={arrowLeft} alt="arrow left" />
-  //         </div>
-  //         <div className="current">{props.currentPage.toUpperCase()}</div>
-  //         <div>all breeds</div>
-  //       </div>
-  //     </div>
-  //   )
-
-  // if (props.currentPage === 'gallery')
-  //   return (
-  //     <div className="content ">
-  //       <div className="flex ">
-  //         <div className="button-back">
-  //           <img src={arrowLeft} alt="arrow left" />
-  //         </div>
-  //         <div className="current">{props.currentPage.toUpperCase()}</div>
-  //         <div>all gallery</div>
-  //       </div>
-  //     </div>
-  //   )
 }
