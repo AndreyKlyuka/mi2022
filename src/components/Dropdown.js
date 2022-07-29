6041d038db3dcf622ae9f1e039d20d2814857dc6
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-07-29 23:25:07 +0300

    feat: work with correct few dropdowns work

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,17 +3,13 @@ import React from 'react'
 export const Dropdown = (props) => {
   return (
     <div
-      className={`absolute top-[60px] w-[300px] h-[${props.height}px] bg-red-400 px-[20px] py-[20px] rounded-[30px]`}
+      className={`absolute top-[60px] w-[300px] ${props.heightTWProps} bg-red-400 px-[20px] py-[20px] rounded-[30px]`}
     >
       <ul className="flex flex-col gap-[10px]">
-        <li>sdfsdf</li>
-        <li>sdfsdffsdf</li>
-        <li>ssdsdfsdsfsdf</li>
-        <li>ssdsdfsdsfsdf</li>
-        <li>sdfsdf</li>
-        <li>sdfsdffsdf</li>
-        <li>ssdsdfsdsfsdf</li>
-        <li>...</li>
+        <li>123123</li>
+        <li>123123</li>
+        <li>123123</li>
+        <li>123123</li>
       </ul>
     </div>
   )
--- src/pages/BreedsPage.js
+++ src/pages/BreedsPage.js
@@ -1,21 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ActionPanel } from '../components/ActionPanel'
 import { ContentSection } from '../components/ContentSection'
 import { Dropdown } from '../components/Dropdown'
 
+import inputArrow from '../assets/input-arrow.svg'
 export const BreedsPage = () => {
+  const [dropdown, setDropdown] = useState(null)
+  const [breedsFlag, setbreedsFlag] = useState(false)
+  const [limitFlag, setlimitFlag] = useState(false)
+
+  const dropdownHandler = (e) => {
+    // setDropdown(
+    //   e.currentTarget.className.toString().includes('breeds')
+    //     ? 'breeds'
+    //     : 'limit'
+    // )
+    if (e.currentTarget.className.toString().includes('breeds')) {
+      setbreedsFlag(true)
+      if (breedsFlag === true) {
+        setbreedsFlag(false)
+        // setlimitFlag(true)
+      }
+      setDropdown('breeds')
+    } else {
+      setlimitFlag(true)
+      if (limitFlag === true) {
+        setlimitFlag(false)
+        // setbreedsFlag(true)
+      }
+      setDropdown('limit')
+    }
+  }
+
+  useEffect(() => {
+    console.log(breedsFlag)
+    console.log(limitFlag, 'limit')
+  })
+
   return (
     <>
       <ActionPanel />
       <ContentSection currentPage={'breeds'}>
-        <div className="ml-[10px] flex gap-[10px]">
-          <div className="relative w-[226px] breeds__input">
-            all breeds
-            <Dropdown height={'300'} />
+        <div className="relative ml-[10px] flex gap-[10px]">
+          <div className="breeds__input" onClick={dropdownHandler}>
+            <div className="flex justify-between w-full my-[8px] mx-[10px]">
+              <div className="w-full">{'All breeds'}</div>
+
+              <div
+                className={
+                  breedsFlag
+                    ? 'flex justify-center items-center rotate-180'
+                    : 'flex justify-center items-center'
+                }
+              >
+                <img src={inputArrow} alt="" />
+              </div>
+            </div>
+            {dropdown === 'breeds' && breedsFlag && (
+              <Dropdown heightTWProps={'h-[300px]'} />
+            )}
           </div>
-          <div className="w-[101px] breeds__input">limit</div>
-          <div className="w-[40px] breeds__input">a-b</div>
-          <div className="w-[40px] breeds__input">b-a</div>
+
+          <div className="limit__input" onClick={dropdownHandler}>
+            <div className="flex justify-between w-full my-[8px] mx-[10px]">
+              <div className="w-full">{'Limit: 10'}</div>
+
+              <div
+                className={
+                  limitFlag
+                    ? 'flex justify-center items-center rotate-180'
+                    : 'flex justify-center items-center'
+                }
+              >
+                <img src={inputArrow} alt="" />
+              </div>
+            </div>
+            {dropdown === 'limit' && limitFlag && (
+              <Dropdown heightTWProps={'h-[164px]'} />
+            )}
+          </div>
+
+          <div className="breeds__sort">a</div>
+          <div className="breeds__sort">b</div>
         </div>
       </ContentSection>
     </>
